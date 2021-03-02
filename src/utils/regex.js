2a7bc98eb150ca302e1c8b71de7b31e07e38045d
Lucas Oliveira <lucasdesousa@terra.com.br>
2021-03-01 22:38:05 -0300

    FEAT (utils) Added regex value testing for use in some components

---
+++ src/utils/regex.js
@@ -0,0 +1,4 @@
+export function verifyUrl(url) {
+  const expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+  return new RegExp(expression).test(url);
+}
