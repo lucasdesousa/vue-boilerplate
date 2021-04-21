a8d1536003767b1b88788fef7736ed28c2838569
Lucas Oliveira <lucasdesousa@terra.com.br>
2021-04-21 14:02:31 -0300

    FEAT (core) Added multi-theme support for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: "var(--primary-color)",
+        "primary-text": "var(--primary-text-color)",
+        secondary: "var(--secondary-color)",
+        "secondary-text": "var(--secondary-text-color)",
+        neutral: "var(--neutral-color)",
+        link: "var(--link-color)",
+        content: "var(--content-background-color)",
+        heading: "var(--heading-background-color)",
+        navbar: "var(--navbar-background-color)",
+        sidebar: "var(--sidebar-background-color)"
+      }
+    }
   },
   variants: {
     extend: {
